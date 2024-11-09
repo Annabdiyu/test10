8a57cea523b992ed070b819d900ed26f61bba81f
Anna Abdiyu <annaply156@gmail.com>
2024-11-09 22:06:18 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,15 +5,14 @@ class Author(models.Model):
     name=models.CharField(max_length=100)
     
     
+    
 class Book(models.Model):
     title=models.CharField(max_length=100)
     author=models.ForeignKey(Author,on_delete=models.CASCADE,related_name='Books')
     
-    
 class Library(models.Model):
     name=models.CharField(max_length=100)
     books=models.ManyToManyField(Book)
-    
     
 class Librarian(models.Model):
     name=models.CharField(max_length=100)
