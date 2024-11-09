bbf9983982fa34f414c7aa80df213adb7652e4f6
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 01:21:55 +0300

    fourth commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
 ]
 
