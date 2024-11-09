1b1c8a76df5013fffb490cb40880b7156c25687c
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 00:28:57 +0300

    third commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView 
+from .views import register, login_view, logout_view
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'), 
+    path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'), 
 ]
