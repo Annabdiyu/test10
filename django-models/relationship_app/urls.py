42101c3d502d09eca0b49a5b336a409a7579b28b
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 00:50:46 +0300

    fourth commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,16 @@
 
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import list_books, LibraryDetailView 
-from .views import register, LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'), 
     path('register/', views.register, name='register'),  # Registration view
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Login view
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Logout view 
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 ]
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,7 +10,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
 
-
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
 def list_books(request):
     books = Book.objects.all()  
@@ -55,3 +56,26 @@ def logout_view(request):
     return render(request, 'relationship_app/logout.html')
 
 
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
