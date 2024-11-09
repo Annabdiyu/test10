ec7d0bdde884dd3b91b52bd85d2d1ac122d9e2dc
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 01:39:09 +0300

    fourth commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,9 +8,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-
-
 from django.contrib.auth.decorators import user_passes_test
+
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect
+from .models import UserProfile
 
 
 def list_books(request):
@@ -61,8 +63,10 @@ def logout_view(request):
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
+
 def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
 
 def is_member(user):
     return user.userprofile.role == 'Member'
@@ -71,7 +75,8 @@ def is_member(user):
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(is_librarian, login_url='/unauthorized/')
+@login_required
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
