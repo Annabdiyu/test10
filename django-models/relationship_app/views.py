fa7c69dbddc3c1934cdaeef0a973f3d3fb6265bc
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 17:57:12 +0300

    ff

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -77,10 +77,14 @@ def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 
-
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
