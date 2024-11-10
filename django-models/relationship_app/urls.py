5409a8926c834f5758027ef179d1401148a57152
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 17:17:07 +0300

    week 10 project

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,12 +11,9 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-    path('book/add/', views.add_book, name='add_book'),
-
-    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
-
-    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
-
+    path('books/', views.list_books, name='book_list'),
+    path('add_book/', views.add_book, name='add_book'),  # Ensure trailing slash
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # Ensure trailing slash
+    path('delete/<int:pk>/', views.delete_book, name='delete_book'),  # Ensure trailing slash
 ]
-
 
