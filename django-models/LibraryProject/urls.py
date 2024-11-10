27b8f815c761ad8ef8424d6474b882e60422f6d5
Anna Abdiyu <annaply156@gmail.com>
2024-11-10 17:22:59 +0300

    week 10 project

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from relationship_app import views as relationship_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
